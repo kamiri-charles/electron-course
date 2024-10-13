e6a1d0a5ee6b1fb940135bf7017cbe3f8991a6f7
Charles Kamiri <charleskamiri6@gmail.com>
2024-10-13 14:57:57 +0300

    feat: Configure electron and electron builder

---
+++ src/electron/main.ts
@@ -0,0 +1,9 @@
+import { BrowserWindow, app } from "electron";
+import path from "path";
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({});
+
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+
+});
